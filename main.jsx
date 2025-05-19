93300b39d9bcaa34fee75b5f76249894386aaa43
Khalid1527 <Kaalharbi.1999@gmail.com>
2025-05-19 23:21:13 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MahasinTech from './MahasinTech'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MahasinTech />
-  </React.StrictMode>
-)
